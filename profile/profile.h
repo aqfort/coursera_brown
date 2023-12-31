e1d90656f85154809c812b89b2487e49864f60c5
aqfort <aqfort.cpp@gmail.com>
2023-12-31 17:41:39 +0300

    Profile

--- profile/profile.h
+++ profile/profile.h
@@ -1,45 +1,40 @@
 #pragma once
 
-#include <chrono>
 #include <iostream>
+#include <chrono>
 #include <string>
-
-using namespace std;
-using namespace std::chrono;
 
 class LogDuration
 {
 public:
-    explicit LogDuration(const string &msg = "")
-        : message(msg + ": "), start(steady_clock::now())
-    {
-    }
-
+    explicit LogDuration(const std::string &msg = std::string{})
+        : message(msg), start(std::chrono::steady_clock::now()) {}
     ~LogDuration()
     {
-        auto finish = steady_clock::now();
+        auto finish = std::chrono::steady_clock::now();
         auto dur = finish - start;
-
-        auto s = duration_cast<seconds>(dur);
+        auto s = duration_cast<std::chrono::seconds>(dur);
         dur -= s;
-        auto ms = duration_cast<milliseconds>(dur);
+        auto ms = duration_cast<std::chrono::milliseconds>(dur);
         dur -= ms;
-        auto us = duration_cast<microseconds>(dur);
+        auto us = duration_cast<std::chrono::microseconds>(dur);
         dur -= us;
-        auto ns = duration_cast<nanoseconds>(dur);
+        auto ns = duration_cast<std::chrono::nanoseconds>(dur);
         dur -= ns;
-
-        cerr << message << "\n\t"
-             << s.count() << " s "
-             << ms.count() << " ms "
-             << us.count() << " us "
-             << ns.count() << " ns "
-             << endl;
+        if (!message.empty())
+        {
+            std::cerr << message << std::endl;
+        }
+        std::cerr << s.count() << " s "
+                  << ms.count() << " ms "
+                  << us.count() << " us "
+                  << ns.count() << " ns "
+                  << std::endl;
     }
 
 private:
-    string message;
-    steady_clock::time_point start;
+    std::string message;
+    std::chrono::steady_clock::time_point start;
 };
 
 #define UNIQ_ID_IMPL(line_no) _a_local_var_##line_no
